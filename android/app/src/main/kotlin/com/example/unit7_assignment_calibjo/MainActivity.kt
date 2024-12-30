d55e46e411b962e79bec1afff5f21407252063ad
JOHNfreedom1234 <joshuaprinze.calibjo@wvsu.edu.ph>
2024-12-31 01:46:26 +0800

    Wait what?

---
+++ android/app/src/main/kotlin/com/example/unit7_assignment_calibjo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.unit7_assignment_calibjo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/about_me.dart
@@ -0,0 +1,35 @@
+import 'package:unit7_assignment_calibjo/components/tab_widget_1.dart';
+import 'package:unit7_assignment_calibjo/components/tab_widget_2.dart';
+import 'package:flutter/material.dart';
+
+class About_Me extends StatefulWidget {
+  const About_Me({super.key});
+
+  @override
+  State<About_Me> createState() => _About_MeState();
+}
+
+class _About_MeState extends State<About_Me> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Hi!"),
+          backgroundColor: Colors.lightBlue,
+          bottom: TabBar(tabs: [
+            Tab(icon: Icon(Icons.contact_page)),
+            Tab(icon: Icon(Icons.settings)),
+          ]),
+        ),
+        body: TabBarView(
+          children: [
+            TabWidget1(),
+            TabWidget2(),
+          ],
+        ),
+      ),
+    );
+  }
+}
